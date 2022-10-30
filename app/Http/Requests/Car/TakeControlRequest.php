cdb0d9e206ebbec4ba41244945b981c8fc81bdce
rammtw <rammtw@users.noreply.github.com>
2022-10-30 10:12:49 +0000

    Apply php-cs-fixer changes

--- app/Http/Requests/Car/TakeControlRequest.php
+++ app/Http/Requests/Car/TakeControlRequest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Requests\Car;
 
 use Illuminate\Foundation\Http\FormRequest;
@@ -24,7 +26,6 @@ class TakeControlRequest extends FormRequest
     public function rules()
     {
         return [
-            //
         ];
     }
 }
