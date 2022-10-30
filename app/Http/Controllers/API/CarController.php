74ebdb1a3a2c89b9d965e854e61a886b9b82285d
Ramil <rammtw@gmail.com>
2022-10-30 16:58:19 +0300

    swagger docs

--- app/Http/Controllers/API/CarController.php
+++ app/Http/Controllers/API/CarController.php
@@ -26,10 +26,26 @@ class CarController extends Controller
      *         description="Pass user id",
      *              @OA\JsonContent(
      *              required={"user_id"},
-     *          @OA\Property(property="user_id", type="integer", example="1"),
-     *    ),
-     * ),
-     *     @OA\Response(response="200", description="Request to take control of car")
+     *              @OA\Property(property="user_id", type="integer", example="1"),
+     *          ),
+     *      ),
+     *      @OA\Response(response="200", description="Request to take control of car"),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Car is under control!",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="data", type="array", example="[]", @OA\Items()),
+     *              @OA\Property(property="error", type="string", example="Car is under control!")
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="The user is already driving a car!",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="data", type="array", example="[]", @OA\Items()),
+     *              @OA\Property(property="error", type="string", example="The user is already driving a car!")
+     *          )
+     *      )
      * )
      */
     public function takeControl(Car $car, TakeControlRequest $request)
@@ -49,7 +65,15 @@ class CarController extends Controller
      *         @OA\Schema(type="integer"),
      *         @OA\Examples(example="int", value="1", summary="An int value."),
      *     ),
-     *     @OA\Response(response="200", description="Request to take off control of car")
+     *     @OA\Response(response="200", description="Request to take off control of car"),
+     *     @OA\Response(
+     *          response=400,
+     *          description="Car is out of control!",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="data", type="array", example="[]", @OA\Items()),
+     *              @OA\Property(property="error", type="string", example="Car is out of control!")
+     *          )
+     *      )
      * )
      */
     public function takeOffControl(Car $car)
